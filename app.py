d6e35abcb89f56db3d6dbce8bb66a11033bc1651
rohith-cse07 <rohith07.cse@gmail.com>
2026-08-07 08:09:05 +0530

    feat:Add a pydantic and validation error framework

--- app.py
+++ app.py
@@ -1,12 +1,70 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from pydantic import BaseModel, Field
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 import pandas as pd
 import joblib
 
-app = FastAPI(title="Rental Price Prediction API")
-
-# Load trained pipeline
+app = FastAPI(
+    title="Rental Price Prediction API",
+    description="API for predicting monthly rental prices",
+    version="1.0.0"
+)
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(
+    request: Request,
+    exc: RequestValidationError
+):
+    return JSONResponse(
+        status_code=422,
+        content={
+            "error": "Validation Error",
+            "details": exc.errors()
+        }
+    )
+# Load model
 model = joblib.load("xgb_model.joblib")
 
+
+# -----------------------------
+# Pydantic Input Schema
+# -----------------------------
+
+class RentalInput(BaseModel):
+
+    city: str
+    city_tier: int = Field(ge=1, le=3)
+
+    property_type: str
+
+    bhk: int = Field(ge=1, le=10)
+    size_sqft: float = Field(gt=0)
+    bathrooms: int = Field(ge=1, le=10)
+    balconies: int = Field(ge=0, le=10)
+
+    furnishing: str
+
+    floor: int = Field(ge=0)
+    total_floors: int = Field(ge=1)
+
+    age_of_property_years: int = Field(ge=0)
+
+    has_parking: int = Field(ge=0, le=1)
+    has_lift: int = Field(ge=0, le=1)
+    has_security: int = Field(ge=0, le=1)
+    has_gym: int = Field(ge=0, le=1)
+    has_pool: int = Field(ge=0, le=1)
+    has_power_backup: int = Field(ge=0, le=1)
+    near_metro: int = Field(ge=0, le=1)
+
+    distance_to_city_center_km: float = Field(ge=0)
+
+    tenant_preference: str
+
+
+# -----------------------------
+# Home Route
+# -----------------------------
 
 @app.get("/")
 def home():
@@ -15,13 +73,24 @@ def home():
     }
 
 
+# -----------------------------
+# Prediction Route
+# -----------------------------
+
 @app.post("/predict")
-def predict(data: dict):
+def predict_rent(data: RentalInput):
 
-    input_data = pd.DataFrame([data])
+    # Convert Pydantic object → Dictionary
+    input_data = data.model_dump()
 
-    prediction = model.predict(input_data)[0]
+    # Dictionary → DataFrame
+    input_df = pd.DataFrame([input_data])
+
+    # Prediction
+    prediction = model.predict(input_df)
+
+    predicted_rent = float(prediction[0])
 
     return {
-        "predicted_rent": float(prediction)
+        "predicted_rent": round(predicted_rent, 2)
     }
